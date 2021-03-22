ef69eacdaabfb7ceab6e55cf3ac129c866d80d16
toshichanapp <toshichanapp@gmail.com>
2021-03-22 15:58:34 +0900

    2

--- 02_object_model/hoge.rb
+++ 02_object_model/hoge.rb
@@ -1,7 +1,16 @@
-class Hoge
+class Hoge < String
   # Hogeクラスの仕様
   # "hoge" という文字列の定数Hogeを持つ
+  Hoge = 'hoge'
+
   # "hoge" という文字列を返すhogehogeメソッドを持つ
+  def hogehoge
+    'hoge'
+  end
+
   # HogeクラスのスーパークラスはStringである
   # 自身が"hoge"という文字列である時（HogeクラスはStringがスーパークラスなので、当然自身は文字列である）、trueを返すhoge?メソッドが定義されている
+  def hoge?
+    self == 'hoge'
+  end
 end
